0435b962defd8cd7ac3ef9a08596b2c9ee846d35
hattagh73 <>
2022-11-19 00:50:57 +0800

    add SecHero

---
+++ components/SecHero.tsx
@@ -0,0 +1,10 @@
+    
+const SecHero = () => {
+    return (
+        <section className="max-w-screen-lg mx-auto py-8">
+            <h1>Unsplash Photos By Categories</h1>
+        </section>
+    )
+}
+export default SecHero;
+    
