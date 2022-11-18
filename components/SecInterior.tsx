d89af7f1a950ae7dfcf473bcd00b6e1473661b86
hattagh73 <>
2022-11-19 00:51:11 +0800

    add SecInterior

---
+++ components/SecInterior.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+
+const SecInterior = () => {
+    return (
+        <section className="h-screen max-w-screen-lg mx-auto">
+
+            <p className="text-4xl">Section</p>
+                    
+            <div className="mt-6 grid grid-cols-4 gap-7 grid-rows-[220px_220px] border">
+                
+                <div className="h-full relative self-center">
+                    {/* <Image
+                        className="object-cover object-center rounded-md"
+                        alt="architecture"
+                        src={i.image+'&'+index}
+                        fill
+                    /> */}
+                </div>
+                
+            </div>
+                    
+        </section>
+    )
+}
+export default SecInterior;
