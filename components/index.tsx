793e52c38772f67b203c54b2a57624b5c3094c20
hattagh73 <>
2022-11-19 00:50:44 +0800

    add components index

---
+++ components/index.tsx
@@ -0,0 +1,7 @@
+//* Layout Component *//
+export { default as Header } from './Header';
+
+//* Section Component *//
+export { default as SecHero } from './SecHero';
+export { default as SecArchitecture } from './SecArchitecture';
+export { default as SecInterior } from './SecInterior';
