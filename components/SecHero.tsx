8c233b527e6ec961a9716a0c7673c3792f41e9ab
hattagh73 <>
2022-11-19 01:56:42 +0800

    update hero

--- components/SecHero.tsx
+++ components/SecHero.tsx
@@ -1,8 +1,15 @@
+import Image from "next/image";
+
     
 const SecHero = () => {
     return (
-        <section className="max-w-screen-lg mx-auto py-8">
-            <h1>Unsplash Photos By Categories</h1>
+        <section className="max-w-screen-lg mx-auto py-8 grid grid-cols-3">
+            <div className="col-span-2">
+                <h1 className="font-medium text-5xl">Landing Page Of Photos By Categories</h1>
+            </div>
+            <div>
+                
+            </div>
         </section>
     )
 }
