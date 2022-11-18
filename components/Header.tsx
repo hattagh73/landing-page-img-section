d1baa4f79df3002d46e317076964a84340daf9f0
hattagh73 <>
2022-11-19 01:56:36 +0800

    update header

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,11 +14,13 @@ const Header = () => {
     }, [])
 
     return (
-        <header className={`sticky top-0 z-[2] bg-white transition ease-linear ${scrollPos > 90 ? 'shadow-md' : 'shadow-none'}`}>
-            <ul className={`max-w-screen-lg mx-auto flex gap-6 py-4 transition ease-linear duration-700 ${scrollPos > 90 ? 'border-b-0' : 'border-b'}`}>
-                <li>Interior</li>
-                <li>Nature</li>
-                <li>Food</li>
+        <header className={`sticky top-0 z-[2] bg-white transition-transform ease-linear duration-700 ${scrollPos > 90 ? 'shadow-md' : 'shadow-none'}`}>
+            <ul className={`max-w-screen-lg mx-auto flex gap-12 transition-transform ease-linear duration-700 ${scrollPos > 90 ? 'border-b-0' : 'border-b'}`}>
+                {   ['Architecture', 'Interior', 'People'].map((i, index) => 
+                    <li key={index} className={`py-4 ${i === 'Architecture' && 'border-b-2 border-grey-800' } `}>
+                        {i}
+                    </li>
+                )}
             </ul>
         </header>
     )
