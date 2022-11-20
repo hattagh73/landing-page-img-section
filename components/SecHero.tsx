ca1211156a06c9ed9f6e86065f2b8f9952719a05
hattagh73 <>
2022-11-20 23:24:20 +0800

    update sec-hero

--- components/SecHero.tsx
+++ components/SecHero.tsx
@@ -5,7 +5,10 @@ const SecHero = () => {
     return (
         <section className="max-w-screen-lg mx-auto py-8 grid grid-cols-3">
             <div className="col-span-2">
-                <h1 className="font-medium text-5xl">Landing Page Of Photos By Categories</h1>
+                <h1 className="font-semibold text-5xl">Landing Page Of Photos By Categories</h1>
+                <p className="mt-6 text-sm">
+                    All these photos are from <span className="italic underline"><a href="https://unsplash.com/">Unsplash</a></span>
+                </p>
             </div>
             <div>
                 
