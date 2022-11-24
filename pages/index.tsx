ce96a61f4658143d2a69f67663983282830a3bec
hattagh73 <>
2022-11-24 23:35:59 +0800

    update index-soon will fetch from unsplash api

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,44 +4,7 @@ import Image from 'next/image';
 import { Header, SecArchitecture, SecHero, SecInterior } from '../components';
 
 const Home:NextPage = () => {
-    const unsplash = 
-    {
-        architecture: [
-            {
-                image: `https://source.unsplash.com/1600x900/?architecture`
-            },
-            {
-                image: `https://source.unsplash.com/1600x900/?architecture`
-            },
-            {
-                image: `https://source.unsplash.com/1600x900/?architecture`
-            },
-            {
-                image: `https://source.unsplash.com/1600x900/?architecture`
-            },
-            {
-                image: `https://source.unsplash.com/1600x900/?architecture`
-            },
-            {
-                image: `https://source.unsplash.com/1600x900/?architecture`
-            }
-        ],
-        interior: [
-            {
-                image: `https://source.unsplash.com/random/1920x1080/?interior`
-            },
-            {
-                image: `https://source.unsplash.com/random/1920x1080/?interior`
-            },
-            {
-                image: `https://source.unsplash.com/random/1920x1080/?interior`
-            },
-            {
-                image: `https://source.unsplash.com/random/1920x1080/?interior`
-            },
-        ]
-    }
-    
+    // `https://source.unsplash.com/1600x900/?architecture`
     return (
         <main>
             <Head><title>Home</title></Head>
@@ -53,3 +16,14 @@ const Home:NextPage = () => {
     )
 }
 export default Home;
+
+// export const getServerSideProps = async () => {
+//     const response = await fetch (`https://api.unsplash.com/photos?client_id=${process.env.UNSPLASH_API_KEY}`);
+//     const data = await response.json();
+
+//     return {
+//         props: {
+//             data: data
+//         }
+//     }
+// }
