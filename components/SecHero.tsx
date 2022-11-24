26337e2a63876d903fd811cec285ff139a28c825
hattagh73 <>
2022-11-24 23:03:19 +0800

    update hero

--- components/SecHero.tsx
+++ components/SecHero.tsx
@@ -5,16 +5,17 @@ const SecHero = () => {
     return (
         <section className="max-w-screen-lg mx-5 lg:mx-auto py-8 grid lg:grid-cols-3">
             {/* Hero Col One */}
-            <div className="col-span-2">
+            <div className="col-span-2 self-center">
                 <h1 className="font-semibold text-5xl">Landing Page Of Photos By Categories</h1>
                 <p className="mt-6 text-sm">
                     Photos are from <span className="italic underline"><a href="https://unsplash.com/">Unsplash</a></span>
                 </p>
             </div>
             {/* Hero Col Two */}
-            <div className="">
-                <div className="-rotate-3 bg-slate-100 w-7/12 p-2">
-                    <Image 
+            <div className="lg:justify-self-end">
+                <div className="-rotate-3 bg-slate-200 w-fit p-2 rounded-md">
+                    <Image
+                        className="rounded-sm" 
                         src={`https://images.unsplash.com/photo-1470042100013-aff2a8f93778?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80`}
                         width="208"
                         height="200"
