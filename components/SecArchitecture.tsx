6df5e9a95945f6fcefdf779d9da8cd30cf5248a0
hattagh73 <>
2022-11-24 23:36:20 +0800

    update section architecture

--- components/SecArchitecture.tsx
+++ components/SecArchitecture.tsx
@@ -12,15 +12,16 @@ const SecArchitecture = () => {
             <div className="grid grid-cols-4 gap-7 grid-rows-[220px_220px] pt-4">
                 
                 {   [1,2,3,4,5,6].map((i, index) =>
-                    <div className="h-full relative self-center overflow-hidden rounded group" key={index}>
+                    <div className="h-full relative self-center overflow-hidden rounded group bg-slate-200" key={index}>
                         <Image
                             className="object-cover object-center z-[1]"
                             alt={`test`}
                             src={`https://source.unsplash.com/1600x900/?architecture/${i}`}
                             fill
                         />
-                        <div className="group-hover:bg-gradient-to-b from-black/80 via-black/5 to-black/80 absolute z-[2] inset-0">
+                        <div className="group-hover:absolute z-[2] inset-0 group-hover:bg-gradient-to-b from-black/80 via-black/5 to-black/80">
                             <div className="bg-gray-200 h-16 w-16 rounded-full"></div>
+                            <p>test</p>
                         </div>
                     </div>
                 )}
