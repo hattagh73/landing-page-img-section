685723c0263db821f0985a88471873120d137d00
hattagh73 <>
2022-11-24 23:04:51 +0800

    update architecture-edit card with img-profile img

--- components/SecArchitecture.tsx
+++ components/SecArchitecture.tsx
@@ -2,20 +2,28 @@ import Image from 'next/image';
 
 const SecArchitecture = () => {
     return (
-        <section className="h-screen max-w-screen-lg mx-auto" name="Architecture">
+        <section 
+            className="h-screen max-w-screen-lg mx-auto" 
+            name="Architecture"
+        >
 
-            <p className="text-4xl">Section</p>
+            <p className="text-xl pt-4">Photos of Architecture</p>
                     
-            <div className="mt-6 grid grid-cols-4 gap-7 grid-rows-[220px_220px] border">
+            <div className="grid grid-cols-4 gap-7 grid-rows-[220px_220px] pt-4">
                 
-                <div className="h-full relative self-center">
-                    {/* <Image
-                        className="object-cover object-center rounded-md"
-                        alt="architecture"
-                        src={i.image+'&'+index}
-                        fill
-                    /> */}
-                </div>
+                {   [1,2,3,4,5,6].map((i, index) =>
+                    <div className="h-full relative self-center overflow-hidden rounded group" key={index}>
+                        <Image
+                            className="object-cover object-center z-[1]"
+                            alt={`test`}
+                            src={`https://source.unsplash.com/1600x900/?architecture/${i}`}
+                            fill
+                        />
+                        <div className="group-hover:bg-gradient-to-b from-black/80 via-black/5 to-black/80 absolute z-[2] inset-0">
+                            <div className="bg-gray-200 h-16 w-16 rounded-full"></div>
+                        </div>
+                    </div>
+                )}
                 
             </div>
                     
@@ -23,3 +31,4 @@ const SecArchitecture = () => {
     )
 }
 export default SecArchitecture;
+
