663a9e9b97b71127bb7f1f4395f38727eb7dec70
hattagh73 <>
2022-11-28 01:44:46 +0800

    add-people-component

--- components/index.tsx
+++ components/index.tsx
@@ -5,6 +5,7 @@ export { default as Header } from './Header';
 export { default as SecHero } from './SecHero';
 export { default as SecArchitecture } from './SecArchitecture';
 export { default as SecInterior } from './SecInterior';
+export { default as SecPeople } from './SecPeople';
 
 //* Reusable Component *//
 export { default as Card } from './Card';
