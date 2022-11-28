995428c4efa1d2f761e166ddf01d6a732d51334f
hattagh73 <>
2022-11-29 00:02:57 +0800

    add-config-file-to-store-api-url-and-key

---
+++ config.ts
@@ -0,0 +1,12 @@
+const API_URL: string = 'https://api.unsplash.com';
+const API_KEY: string | undefined = process.env.UNSPLASH_API_KEY;
+
+export {
+    API_URL,
+    API_KEY
+}
+
+/* 
+    * Nice configuration setup that we found, big thanks to: 
+    * https://github.com/weibenfalk/nextjs-13-refactor/blob/main/config.ts
+*/
